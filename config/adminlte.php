6402708dd9f824fe1c887c5d116d7e3a2502abd3
UriGood <101681783+urielmf@users.noreply.github.com>
2022-03-19 14:23:04 -0600

    corrección name

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Inventario Uriel Martínez </b>',
+    'logo' => '<b>Inventario Uriel Mtz </b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
